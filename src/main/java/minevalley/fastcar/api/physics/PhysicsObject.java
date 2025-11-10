8853a3d4fc9a68e1fc1ae0651c32dbe449088bf7
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2025-11-10 01:26:43 +0100

    chore(PhysicsObject): add some useful methods

--- src/main/java/minevalley/fastcar/api/physics/PhysicsObject.java
+++ src/main/java/minevalley/fastcar/api/physics/PhysicsObject.java
@@ -1,25 +1,43 @@
 package minevalley.fastcar.api.physics;
 
+import minevalley.core.api.users.OnlineUser;
 import minevalley.fastcar.api.vehicle.Vehicle;
 import org.bukkit.Location;
 import org.jetbrains.annotations.Contract;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Set;
 
 @SuppressWarnings("unused")
 public interface PhysicsObject<T extends Vehicle<T>> {
 
+    /**
+     * Get the vehicle associated with this physics object.
+     *
+     * @return the vehicle
+     */
     @Nonnull
     @Contract(pure = true)
     T vehicle();
 
     /**
-     * Get the speed of the object in km/h.
+     * Get the driver of the vehicle, if any.
      *
-     * @return the speed in km/h
+     * @return the driver, or null if there is no driver
      */
+    @Nullable
     @Contract(pure = true)
-    double getSpeed();
+    OnlineUser getDriver();
+
+    /**
+     * Get the passengers of the vehicle, including the driver.
+     *
+     * @return the set of passengers
+     */
+    @Nonnull
+    @Contract(pure = true)
+    Set<OnlineUser> getPassengers();
 
     /**
      * Get the current location of the object.
@@ -29,4 +47,12 @@ public interface PhysicsObject<T extends Vehicle<T>> {
     @Nonnull
     @Contract(pure = true)
     Location getLocation();
+
+    /**
+     * Get the speed of the object in km/h.
+     *
+     * @return the speed in km/h
+     */
+    @Contract(pure = true)
+    float getSpeed();
 }
