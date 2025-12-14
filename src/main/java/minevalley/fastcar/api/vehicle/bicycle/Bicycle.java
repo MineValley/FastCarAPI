5db35774495ff8eb2ff2a0ca47828e10de676fc1
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2025-12-14 01:58:31 +0100

    chore: add model() to bicycle

--- src/main/java/minevalley/fastcar/api/vehicle/bicycle/Bicycle.java
+++ src/main/java/minevalley/fastcar/api/vehicle/bicycle/Bicycle.java
@@ -1,7 +1,16 @@
 package minevalley.fastcar.api.vehicle.bicycle;
 
+import minevalley.fastcar.api.production.bicycle.BicycleModel;
 import minevalley.fastcar.api.vehicle.Vehicle;
+import org.jetbrains.annotations.Contract;
+
+import javax.annotation.Nonnull;
 
 @SuppressWarnings("unused")
 public interface Bicycle extends Vehicle<Bicycle> {
+
+    @Override
+    @Nonnull
+    @Contract(pure = true)
+    BicycleModel model();
 }
