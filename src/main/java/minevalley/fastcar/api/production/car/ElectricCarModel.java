bce4e132d2ff0128a5ebad1310d99453fa9b5c44
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2026-01-08 05:30:56 +0100

    chore: optimize imports

--- src/main/java/minevalley/fastcar/api/production/car/ElectricCarModel.java
+++ src/main/java/minevalley/fastcar/api/production/car/ElectricCarModel.java
@@ -1,7 +1,6 @@
 package minevalley.fastcar.api.production.car;
 
 import minevalley.fastcar.api.production.RechargeableModel;
-import minevalley.fastcar.api.vehicle.car.CombustionCar;
 import minevalley.fastcar.api.vehicle.car.ElectricCar;
 
 @SuppressWarnings("unused")
