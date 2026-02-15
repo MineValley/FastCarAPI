a4b3696413f40c79e423c8078a3bd25e5facd1a5
Snabeldier <login@vincent-boch.de>
2026-02-15 20:54:18 +0100

    docs(ShapeProperty): add javadoc and contracts

--- src/main/java/minevalley/fastcar/api/production/shape/ShapeProperty.java
+++ src/main/java/minevalley/fastcar/api/production/shape/ShapeProperty.java
@@ -1,15 +1,28 @@
 package minevalley.fastcar.api.production.shape;
 
 import minevalley.core.api.utils.ItemBuilder;
+import org.jetbrains.annotations.Contract;
 
 import javax.annotation.Nonnull;
 
 @SuppressWarnings("unused")
 public interface ShapeProperty {
 
+    /**
+     * Gets the display name of this shape property, which is used for example in the production menu.
+     *
+     * @return the display name of this shape property
+     */
     @Nonnull
+    @Contract(pure = true)
     String getDisplayName();
 
+    /**
+     * Gets the item representation of this shape property, which is used for example in the production menu.
+     *
+     * @return the item representation of this shape property
+     */
     @Nonnull
+    @Contract(pure = true)
     ItemBuilder getItem();
 }
