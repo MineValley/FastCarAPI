5d16586ba595b0ac9d6455a07eeab0a6f2c679a5
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2026-02-25 03:06:57 +0100

    feat(Attachment): add name()

--- src/main/java/minevalley/fastcar/api/production/shape/Attachment.java
+++ src/main/java/minevalley/fastcar/api/production/shape/Attachment.java
@@ -1,5 +1,19 @@
 package minevalley.fastcar.api.production.shape;
 
+import org.jetbrains.annotations.Contract;
+
+import javax.annotation.Nonnull;
+
 @SuppressWarnings("unused")
 public interface Attachment extends ShapeProperty {
+
+    /**
+     * Gets the name of this attachment.
+     * This corresponds to the database name of the attachment and is used to identify it.
+     *
+     * @return The attachment name.
+     */
+    @Nonnull
+    @Contract(pure = true)
+    String name();
 }
