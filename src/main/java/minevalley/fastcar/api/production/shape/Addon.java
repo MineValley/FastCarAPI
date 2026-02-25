60fc3fd3143e0b109ca2d759dd8f7d27dcb803b6
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2026-02-25 02:29:42 +0100

    chore: remove vehicle components and merge into addons

--- src/main/java/minevalley/fastcar/api/production/shape/Addon.java
+++ src/main/java/minevalley/fastcar/api/production/shape/Addon.java
@@ -21,7 +21,9 @@ public enum Addon implements Attachment {
     RIGHT_BIKE_SLOT("Rechte Fahrradbefestigung",
             "12a00cd216132735516671116eba3aa150c206b61acdd3dfa54c03bd7e45cc63"),
     TRAILER_HITCH("Anhängerkupplung",
-            "c411d48eebe3b1a327ad5fefe48b9852f8a449930c9b2fedaeef2d5f7ad20a6e");
+            "c411d48eebe3b1a327ad5fefe48b9852f8a449930c9b2fedaeef2d5f7ad20a6e"),
+    AIR_SCREW("Propeller",
+            "1b52027c71f6ec38426cc2bb75d8e67b91dc82869ffdcb70efdf513d6325f62e");
 
     @Getter
     @Nonnull
