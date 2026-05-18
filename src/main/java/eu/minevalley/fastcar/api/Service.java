4c17161550b13ce4e03f1c79ec76f0195035906b
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2026-05-18 20:14:02 +0200

    docs(Service): add javadoc

--- src/main/java/eu/minevalley/fastcar/api/Service.java
+++ src/main/java/eu/minevalley/fastcar/api/Service.java
@@ -12,20 +12,48 @@ import javax.annotation.Nonnull;
 @SuppressWarnings("unused")
 public interface Service {
 
+    /**
+     * Creates a visual builder which can then be built into a visual.
+     *
+     * @param body body to define the looking of the visual from
+     * @return newly created visual builder
+     * @throws IllegalArgumentException if the body is null
+     */
     @Nonnull
     @Contract("_ -> new")
     VisualBuilder createVisual(@Nonnull VehicleBody body) throws IllegalArgumentException;
 
+    /**
+     * Creates a temporary vehicle that will be automatically removed upon the next restart.
+     *
+     * @param model              model of the vehicle
+     * @param licensePlatePrefix the prefix of the license plate. Based on that the system creates a license plate. This is not supposed to be MV!
+     * @param color              color of the vehicle
+     * @param <T>                the type of vehicle (VehicleClass)
+     * @return newly created (but not yet spawned!) vehicle
+     * @throws IllegalArgumentException if the model, the licensePlatePrefix or the color is null, if the licensePlate does not exist of exactly 2 characters or if the color is not supported by the model.
+     */
     @Nonnull
     @Contract("_, _, _ -> new")
     <T extends Vehicle<T>> Vehicle<T> createTemporaryVehicle(@Nonnull Model<T> model,
                                                              @Nonnull String licensePlatePrefix,
-                                                             @Nonnull VehicleColor color);
+                                                             @Nonnull VehicleColor color)
+            throws IllegalArgumentException;
 
+    /**
+     * Creates a temporary vehicle that will be automatically removed upon the next restart.
+     *
+     * @param model              model of the vehicle
+     * @param licensePlatePrefix the prefix of the license plate. Based on that the system creates a license plate. This is not supposed to be MV!
+     * @param <T>                the type of vehicle (VehicleClass)
+     * @return newly created (but not yet spawned!) vehicle
+     * @throws IllegalArgumentException if the model, the licensePlatePrefix or the color is null or if the licensePlate does not exist of exactly 2 characters.
+     */
     @Nonnull
     @Contract("_, _ -> new")
     default <T extends Vehicle<T>> Vehicle<T> createTemporaryVehicle(@Nonnull Model<T> model,
-                                                                     @Nonnull String licensePlatePrefix) {
+                                                                     @Nonnull String licensePlatePrefix)
+            throws IllegalArgumentException {
         return createTemporaryVehicle(model, licensePlatePrefix, model.body().defaultColors()[0]);
     }
 }
