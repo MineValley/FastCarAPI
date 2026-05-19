1d4bcdeb34ec69a7c1284a6cabb0371200b3d2ac
Snabeldier <vincegamezspam@gmail.com>
2026-05-19 10:42:49 +0200

    feat(CarModel): extend PoweredModel

--- src/main/java/eu/minevalley/fastcar/api/characteristics/type/car/CarModel.java
+++ src/main/java/eu/minevalley/fastcar/api/characteristics/type/car/CarModel.java
@@ -1,9 +1,10 @@
 package eu.minevalley.fastcar.api.characteristics.type.car;
 
 import eu.minevalley.fastcar.api.characteristics.Model;
+import eu.minevalley.fastcar.api.characteristics.specs.PoweredModel;
 import eu.minevalley.fastcar.api.characteristics.specs.StorageModel;
 import eu.minevalley.fastcar.api.vehicle.car.Car;
 
-public interface CarModel<T extends Car<T>> extends Model<T>, StorageModel {
+public interface CarModel<T extends Car<T>> extends Model<T>, StorageModel, PoweredModel {
 
 }
